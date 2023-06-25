d88dc226911fd0b1ef4d16b08e714f37ef864f1d
Viewbell <128227140+Viewbell@users.noreply.github.com>
2023-06-25 08:55:41 +0200

    Add files via upload

---
+++ Ćwiczenie_2.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double ZwrocDlugosc(
+        double[] punkt1, //x1 y1 
+        double[] punkt2  //x2 y2 
+        )
+        {
+            return Math.Sqrt(
+                    Math.Pow((punkt2[0] - punkt1[0]), 2) +
+                    Math.Pow((punkt2[1] - punkt1[1]), 2)
+                 );
+        }
+
+
+        Console.WriteLine("Podaj współrzędne punktu 1: ");
+        double[] punkt1 = new double[]
+        {
+    double.Parse(Console.ReadLine()),
+    double.Parse(Console.ReadLine())
+        };
+
+        Console.WriteLine("Podaj współrzędne punktu 2: ");
+        double[] punkt2 = new double[]
+        {
+    double.Parse(Console.ReadLine()),
+    double.Parse(Console.ReadLine())
+        };
+
+        Console.WriteLine($"Długość odcinka to: {ZwrocDlugosc(punkt1, punkt2)}");
+    }
+}
---
+++ Ćwiczenie_3.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double podajMediane(double[] tablicaPodana)
+        {    
+            if (tablicaPodana == null || tablicaPodana.Length == 0)
+                throw new ArgumentException("Tablica jest pusta.");
+
+            double[] tablicaPosortowana = (double[])tablicaPodana.Clone();
+            Array.Sort(tablicaPosortowana);
+
+            int rozmiar = tablicaPosortowana.Length;
+            int srodek = rozmiar / 2;
+
+            if (rozmiar % 2 != 0)
+                return tablicaPosortowana[srodek];
+
+            dynamic value1 = tablicaPosortowana[srodek];
+            dynamic value2 = tablicaPosortowana[srodek - 1];
+            return (value1 + value2) / 2;
+        }
+
+        Console.WriteLine("Podaj wielkość tablicy: ");
+        int wielkoscTablicy = int.Parse(Console.ReadLine());
+        double[] tablica = new double[wielkoscTablicy];
+
+        for (int i = 0; i < tablica.Length; i++)
+        {
+            Console.WriteLine($"Pozycja {i + 1} ma się równać: ");
+            tablica[i] = double.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"Mediana dla tablicy to: {podajMediane(tablica)}");
+    }
+}
